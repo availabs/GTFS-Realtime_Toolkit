4cf1e143cd83bca79a9ef9682e8b48eeac08f711
Paul Tomchik <pauljtomchik@gmail.com>
2015-08-20 19:46:33 -0400

    Generalized.

--- tests/config.js
+++ tests/config.js
@@ -2,8 +2,11 @@
 
 var key           = require('./apiKey'),
 
-    url           = "http://datamine.mta.info/mta_esi.php?key=" + key,
-    protofilePath = __dirname + '/' + './proto_files/nyct-subway.proto';
+    //url           = "http://datamine.mta.info/mta_esi.php?key=" + key,
+    //protofilePath = __dirname + '/' + './proto_files/nyct-subway.proto';
+
+    url           = "http://api.bart.gov/gtfsrt/tripupdate.aspx",
+    protofilePath = __dirname + '/' + './proto_files/gtfs-realtime.proto';
 
 
 module.exports = {
