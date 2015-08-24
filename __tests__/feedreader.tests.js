81ce94a5addb8d81c49d120e03c2b5a921539237
Paul Tomchik <pauljtomchik@gmail.com>
2015-08-24 19:22:55 -0400

    Minor fix in Wrapper.

--- __tests__/feedreader.tests.js
+++ __tests__/feedreader.tests.js
@@ -13,26 +13,5 @@ describe('Simple GTFS-Realtime Wrapper Tests.', function() {
         }
 
         feedreader.registerListener(listener);
-
-        console.log(JSON.stringify(feedreader));
     });
-
-    //it('Build a wrapper from the sample message.', function() {
-        //var FeedReader = require('../lib/FeedReader.js'),
-            //config     = require('./.config.js'),
-            //feedreader = new FeedReader(config);
-
-        //expect(FeedReader).toBeCalledWith(config);
-    //});
 });
-
-
-        //feedReader.registerListener(listener);
-
-        //function listener (msg) {
-            //feedreader.removeListener(listener);
-
-            //console.log(_.filter(_.pluck(msg.entity, 'trip_update.trip.trip_id')));
-        //}
-
-
